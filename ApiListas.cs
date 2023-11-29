ddd534f7329ef1d5874ef26c575dafce4a569e22
Gabriel Nunes <gbrlnns@icloud.com>
2023-11-28 22:43:49 -0300

    Resolvido o bug que fazia com que algumas tarefas aparecessem repetidas.

--- ApiListas.cs
+++ ApiListas.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http.Headers;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace API_Tarefas;
@@ -11,7 +12,7 @@ public static class ApiListas
         var request = new HttpRequestMessage
         {
             Method = HttpMethod.Post,
-            RequestUri = new Uri("https://api.projurisadv.com.br/adv-service/tarefa/consulta-com-paginacao"),
+            RequestUri = new Uri("https://api.projurisadv.com.br/adv-service/tarefa/calendario/consulta-sem-paginacao"),
             Headers =
             {
                 { "accept", "application/json" },
@@ -34,10 +35,21 @@ public static class ApiListas
          * Com a conversão, é possível que o programa manipule as tarefas com base em seus dados cadastrais.
          * No caso do Projuris ADV, apenas a chave "tarefaConsultaWs" é usada, pois é ela que contém os dados de cada tarefa.
          */
-
-        JObject jsonResponse = JObject.Parse(body);
-        JArray tarefasArray = (JArray)jsonResponse["tarefaConsultaWs"];
-        List<Dictionary<string, object?>> tarefasDictList = tarefasArray.Select(item => item.ToObject<Dictionary<string, object>>()).ToList();
+        
+        var jsonResponse = JsonConvert.DeserializeObject<JObject>(body);
+        var tarefasAgrupadasDataTipoWs = jsonResponse["tarefasAgrupadasDataTipoWs"];
+        var tarefasDictList = new List<Dictionary<string, object>>();
+        
+        foreach (var item in tarefasAgrupadasDataTipoWs)
+        {
+            var tarefaConsultaWs = item["tarefaConsultaWs"];
+            foreach (var tarefa in tarefaConsultaWs)
+            {
+                var tarefaDict = JsonConvert.DeserializeObject<Dictionary<string, object>>(tarefa.ToString());
+                tarefasDictList.Add(tarefaDict);
+            }
+        }
+        
         return tarefasDictList;
     }
 }
